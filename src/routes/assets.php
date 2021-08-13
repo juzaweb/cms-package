99b9747983b95e9c0fd1cf828760a87f7a9230fb
Thế Anh Đặng <anh.dang@3forcom.com.vn>
2021-08-13 17:13:06 +0700

    :sparkles: Theme publish assets

---
+++ src/routes/assets.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * MYMO CMS - The Best Laravel CMS
+ *
+ * @package    mymocms/mymocms
+ * @author     The Anh Dang <dangtheanh16@gmail.com>
+ * @link       https://github.com/mymocms/mymocms
+ * @license    MIT
+ *
+ * Created by The Anh.
+ * Date: 8/13/2021
+ * Time: 5:08 PM
+ */
+
+use Juzaweb\Theme\Theme;
+
+Theme::assetsRoutes();
