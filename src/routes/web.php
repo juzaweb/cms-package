da9f65252b77162bb042803e94ea9adf06eb9732
Thế Anh Đặng <anh.dang@3forcom.com.vn>
2021-08-14 15:41:50 +0700

    :bug: Fix generate plugin

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * MYMO CMS - The Best Laravel CMS
+ * JUZAWEB CMS - The Best CMS for Laravel Project
  *
  * @package    juzaweb/laravel-cms
  * @author     The Anh Dang <dangtheanh16@gmail.com>
