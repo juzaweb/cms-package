73ea7b3698eeb21988f4f89242ea1a9bc60ac88b
Juzaweb <dangtheanh16@gmail.com>
2021-09-15 17:20:49 +0700

    :bug: Fix install actions

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -11,5 +11,6 @@ $factory->define(\Juzaweb\Models\User::class, function (Faker $faker) {
         'password' => Hash::make('12345678'),
         'created_at' => $faker->dateTime(),
         'updated_at' => $faker->dateTime(),
+        'is_admin' => 0
     ];
 });
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -6,6 +6,10 @@ class UserTableSeeder extends Seeder
 {
     public function run()
     {
+        factory(\Juzaweb\Models\User::class, 1)->create([
+            'is_admin' => 1
+        ]);
+
         factory(\Juzaweb\Models\User::class, 10)->create();
     }
 }
