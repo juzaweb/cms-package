ff080b70b6867b949e2d03ff1c22dc1335745f9c
Juzaweb <dangtheanh16@gmail.com>
2021-10-14 22:13:13 +0700

    :bug: Fix update manage

--- src/Support/Manager/UpdateManager.php
+++ src/Support/Manager/UpdateManager.php
@@ -116,8 +116,7 @@ class UpdateManager
         switch ($this->tag) {
             case 'core':
                 $response = $this->api->get($uri, [
-                    'current_version' => $this->getCurrentVersion(),
-                    'update_version' => $this->version,
+                    'current_version' => $this->getCurrentVersion()
                 ]);
 
                 break;
